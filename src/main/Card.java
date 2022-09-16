de2807ef9e2937763d64be3a57e24ca200e6de22
justinkeung <justinkeung1018@gmail.com>
2022-09-16 22:01:58 +0800

    Added Javadoc comments

--- src/main/Card.java
+++ src/main/Card.java
@@ -12,14 +12,27 @@ public class Card implements Comparable<Card> {
         this.suit = card.suit;
     }
 
+    /**
+     * Returns the value of the card.
+     * @return The value of the card.
+     */
     public Value value() {
         return value;
     }
 
+    /**
+     * Returns the suit of the card.
+     * @return The suit of the card.
+     */
     public Suit suit() {
         return suit;
     }
 
+    /**
+     * Returns the string representation of the card.
+     * The string is in the format of "[value] of [suit]".
+     * @return The string representation of the card.
+     */
     public String toString() {
         return value.valueName() + " of " + suit.suitName();
     }
