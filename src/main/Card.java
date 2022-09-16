99ba4008344a12f3870bd5b15a071079818a84e5
justinkeung <justinkeung1018@gmail.com>
2022-09-16 21:58:10 +0800

    Cleaned up comments in Card

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,20 +1,10 @@
 public class Card implements Comparable<Card> {
     private final Value value;
     private final Suit suit;
-//    private final ImageIcon image;
-//    private final int x;
-//    private final int y;
 
-    // Constructor
     public Card(Value value, Suit suit) {
         this.value = value;
         this.suit = suit;
-//        this.x = 100; // Width of the card
-//        this.y = (int) (100 * 1.452); // Height of the card
-//
-//        ImageIcon tempImage = new ImageIcon("src/playing_cards/" + value.getValueString().toLowerCase() + "_of_" + suit.getSuitString().toLowerCase() + ".png");
-//        Image resizedImage = tempImage.getImage().getScaledInstance(this.x, this.y, java.awt.Image.SCALE_SMOOTH);
-//        this.image = new ImageIcon(resizedImage);
     }
 
     public Card(Card card) {
@@ -22,7 +12,6 @@ public class Card implements Comparable<Card> {
         this.suit = card.suit;
     }
 
-    // Public methods
     public Value value() {
         return value;
     }
@@ -31,17 +20,11 @@ public class Card implements Comparable<Card> {
         return suit;
     }
 
-//    public ImageIcon getImageIcon() {
-////        System.out.println("src/playing_cards/" + value.getValueString().toLowerCase() + "_of_" + suit.getSuitString().toLowerCase() + ".png");
-//        return image;
-//    }
-
     public String toString() {
         return value.valueName() + " of " + suit.suitName();
     }
 
     @Override
-    // Note that since all cards are unique in the deck, returning 0 implies an error
     public int compareTo(Card card) {
         int thisValueRank = value.rank();
         int thisSuitRank = suit.rank();
